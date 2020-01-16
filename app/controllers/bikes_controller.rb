5affa8d03f6a1300afb3b082d621618c73f9d95d
Julian Sautter <juliansautter1@gmail.com>
2020-01-16 19:51:26 +0100

    adding all action bikes_controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,4 +1,6 @@
 class BikesController < ApplicationController
+  before_actions :find_bike, only:[:create, :show, :edit, :destroy]
+
   def index
     @bikes = Bike.all
   end
@@ -7,17 +9,27 @@ class BikesController < ApplicationController
   end
 
   def new
+    @bike = Bike.new(params[:id])
   end
 
   def create
+    @bike.save
   end
 
   def edit
   end
 
   def update
+    @bike.update(params[:id])
   end
 
   def destroy
+    @bike.destroy
+  end
+
+  private
+
+  def find_bike
+    @bike = Bike.find(params[:id])
   end
 end
