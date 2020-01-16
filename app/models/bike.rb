0216c55f6a9715bcec300750a3ef8a8702dfa441
Gregoire le Hardy <gregoire.lehardy@gmail.com>
2020-01-16 20:05:02 +0100

    first validations

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,9 @@
 class Bike < ApplicationRecord
+  has_many :bookings
+  has_many :users, through: :users
+  has_attachment :photo
+
+  validates :status, presence: true
+  validates :user_id, presence: true
+  validates :address_id, presence: true
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,4 @@
 class Rating < ApplicationRecord
+  belongs_to :bike
+  validates :rating, inclusion: { in: 0..5 }
 end
