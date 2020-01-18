0f817ba16f8ec680a226e90fd845ff6d7d05797b
Gregoire le Hardy <gregoire.lehardy@gmail.com>
2020-01-18 13:30:44 +0100

    cleaning addresses

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create] do
     resources :bikes do
       resources :bookings, only: [:show, :new, :create, :index, :edit]
-      resources :addresses, only:[:show, :new, :create, :index, :edit]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
