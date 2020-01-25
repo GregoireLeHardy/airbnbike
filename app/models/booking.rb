d8214ad5eb8350328301b54c1b892f6b2e9f7195
Benjamin Bruyninx <bbruyninx@hotmail.com>
2020-01-25 14:12:05 +0100

    models adaptations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,8 +6,4 @@ class Booking < ApplicationRecord
   validates :user_id, presence: true
   # validates :start_date, presence: true
   # validates :end_date, presence: true
-
-  def self.status
-    ['Available', 'Pending', 'Booked']
-  end
 end
